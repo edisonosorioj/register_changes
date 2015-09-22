e4bdac293bbf5676cd168d11bceb7d0798605f72
Edison Osorio <edisonosorioj@gmail.com>
2015-09-21 19:02:33 -0500

    Pruebas para mejora de paginación y footer

--- conexion.php
+++ conexion.php
@@ -9,4 +9,24 @@ $link = mysqli_connect($dbhostname,$dbusername, $dbpassword,"cied_core_4") or di
 $query = "SELECT * FROM register_changes_reports order by modification_date DESC" or die("Error in the consult.." . mysqli_error($link)); 
 
 $result = $link->query($query);
+
+//Paginacion
+
+$limit = 12;
+
+$pagina = $_GET["pagina"];
+// if ($pag < 1){
+// 	$pag = 1;
+// }
+// $offset = ($pag-1) * $limit;
+
+// $sql = "SELECT SQL_CALC_FOUND_ROWS * FROM register_changes_reports LIMIT $offset, $limit";
+// $sqlTotal = "SELECT FOUND_ROWS() as total";
+
+// $rs = mysql_query($sql);
+// $rsTotal = mysql_query($sqlTotal);
+
+// $rowTotal = mysql_fetch_array($rsTotal);
+
+// $total = $rowTotal["total"];
 ?>
